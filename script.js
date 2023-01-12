db73d4ea7d19a4db19be47a05f325c5dcabe2ebc
MuditJain31 <muditjain921@gmail.com>
2023-01-13 02:07:33 +0530

    tab functionality added and basic blocks built

---
+++ script.js
@@ -0,0 +1,30 @@
+function execute() {
+    let htmlCode = document.querySelector("#html").value;
+    let cssCode = '<style>' + document.querySelector('#css').value + '</style>';
+    let jsCode = document.querySelector('#js').value;
+    let output = document.querySelector('#output');
+
+    output.contentDocument.body.innerHTML = htmlCode + cssCode;
+    output.contentWindow.eval(jsCode);
+}
+
+document.querySelector("#html").addEventListener("keyup",execute);
+document.querySelector("#css").addEventListener("keyup",execute);
+document.querySelector("#js").addEventListener("keyup",execute);
+
+document.addEventListener("keydown",function(event){
+    // .which is deprecated also it returns diff value for a key in different browser
+    if(event.key==='Tab'){
+        event.preventDefault();
+        let textAreas = document.querySelectorAll("textarea");
+        textAreas.forEach(function(textArea){
+            if(textArea===document.activeElement){
+                const pos = textArea.selectionStart;
+                let val = textArea.value;
+                let newStr = val.slice(0,pos)+"    "+val.slice(pos);
+                textArea.value=newStr;
+                textArea.selectionEnd = pos+4;
+            }
+        })
+    }
+})
