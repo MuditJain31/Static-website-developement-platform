73512d907053ac1172b96393f57b466ae5741731
MuditJain31 <muditjain921@gmail.com>
2023-01-22 01:53:13 +0530

    made output responsive and some styling changes also wrapped frame in div

--- handler.js
+++ handler.js
@@ -1,15 +1,17 @@
 let isLeftDragging = false;
 let isRightDragging = false;
-
+let isVertivalDragging = false;
 function ResetColumnSizes() {
 	// when codeArea resizes return to default col sizes
-	let codeArea = document.querySelector(".codeArea");
+	let codeArea = document.querySelector("#codeArea");
+	let editor = document.querySelector("#editor");
 	codeArea.style.gridTemplateColumns = "1fr 9px 1fr 9px 1fr";
+	// editor.style.gr
 }
 ResetColumnSizes();
 
 function SetCursor(cursor) {
-	let codeArea = document.querySelector(".codeArea");
+	let codeArea = document.querySelector("#codeArea");
 	codeArea.style.cursor = cursor;
 }
 
@@ -19,6 +21,13 @@ function StartLeftDrag() {
 	
 	SetCursor("ew-resize");
 }
+
+// function StartVerticalDrag(){
+// 	console.log("mouse down");
+// 	isVertivalDragging=true;
+
+// 	SetCursor("ns-resize");
+// }
 
 function StartRightDrag() {
 	console.log("mouse down");
@@ -31,25 +40,16 @@ function EndDrag() {
 	console.log("mouse up");
 	isLeftDragging = false;
 	isRightDragging = false;
-	
 	SetCursor("auto");
 }
 
-function StopDrag(){
-	// console.log("mouse up");
-	// isLeftDragging = false;
-	// isRightDragging = false;
-	
-	// SetCursor("auto");
-
-}
 
 function OnDrag(event) {
 	if(isLeftDragging || isRightDragging) {
 		console.log("Dragging");
 		//console.log(event);
 		
-		let codeArea = document.querySelector(".codeArea");
+		let codeArea = document.querySelector("#codeArea");
 		let leftcol = document.getElementById("leftcol");
 		let rightcol = document.getElementById("rightcol");	
 		
@@ -67,10 +67,46 @@ function OnDrag(event) {
 		];
 		
 		let newColDefn = cols.map(c => c.toString() + "px").join(" ");
-			
+		
+		console.log(newColDefn);
+		
+		let dimensionPercentage = newColDefn.split(" ");
+		dimensionPercentage[0] = (cols[0]*100/codeArea.clientWidth)+"%";
+		dimensionPercentage[2] = (cols[2]*100/codeArea.clientWidth)+"%";
+		dimensionPercentage[4] = (cols[4]*100/codeArea.clientWidth)+"%";
+		newColDefn = dimensionPercentage.join(" ")
+		console.log(dimensionPercentage);
 		console.log(newColDefn);
 		codeArea.style.gridTemplateColumns = newColDefn;
 		
 		event.preventDefault();
 	}
 }
+
+// function verticalDrag(event){
+// 	if(isVertivalDragging){
+// 		console.log("yooo");
+// 		let editor = document.querySelector("#editor");
+// 		let codeArea = document.querySelector("#codeArea");
+// 		let frame = document.querySelector("#outputArea");
+// 		let containerOffset = editor.offsetTop;
+// 		let pointerYPos = event.clientY - containerOffset;
+
+// 		let minWidth = 50;
+
+// 		codeArea.style.height = (Math.max(minWidth,pointerYPos-9)) + "px";
+// 		codeArea.style.flexGrow=0;
+// 	}
+// }
+
+window.addEventListener("resize",()=>{
+	let codeArea = document.querySelector("#codeArea");
+	let frame = document.querySelector("iframe");
+	let newWidths = ["calc(100%/3 - 18px/3)","9px","calc(100%/3 - 18px/3)","9px","calc(100%/3 - 18px/3)"];
+	let newCol = newWidths.join(" ");
+	codeArea.style.gridTemplateColumns = newCol;
+
+	codeArea.style.height = "50%";
+	frame.style.height = "50%";
+
+})
